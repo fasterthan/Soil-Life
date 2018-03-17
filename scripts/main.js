0fa1052a61b2c2266ad836bbff6d89b81008046a
Irfan Ainuddin <irfandin@gmail.com>
2018-03-16 18:56:38 -0700

    part 2 basic functions working

--- scripts/main.js
+++ scripts/main.js
@@ -49,3 +49,20 @@ change();
 /* //////////////////////////////////////////// */
 /* /////////// Navbar on Scroll /////////////// */
 /* //////////////////////////////////////////// */
+
+
+$(document).ready(function(){
+   var scroll_start = 0;
+   var startchange = $('.allConnected');
+   var offset = startchange.offset();
+   $(document).scroll(function() {
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top) {
+          $('.navbar').css('background-color', 'rgba(255,255,255,1.0)');
+       } else {
+          $('.navbar').css('background-color', 'rgba(255,255,255,0.7)');
+       }
+   });
+});
+
+/// Sticky Divs ////
