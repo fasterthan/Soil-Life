5b9a5600a956297483eab7d96b91bedf7d8d6d26
Irfan Ainuddin <irfandin@gmail.com>
2018-03-30 23:49:25 -0700

    Added basic views and  header/footer includes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,28 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Soil Life' });
 });
 
+/* GET home page. */
+router.get('/soilCircle', function(req, res, next) {
+  res.render('soilCircle', { title: 'Soil Life' });
+});
+
+/* GET home page. */
+router.get('/sixFs', function(req, res, next) {
+  res.render('sixFs', { title: 'Soil Life' });
+});
+
+/* GET home page. */
+router.get('/soils101', function(req, res, next) {
+  res.render('soils101', { title: 'Soil Life' });
+});
+
+/* GET home page. */
+router.get('/involved', function(req, res, next) {
+  res.render('involved', { title: 'Soil Life' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'Soil Life' });
+});
+
 module.exports = router;
